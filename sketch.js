e0fef78d311748959804c4d0783643d00eccbfaa
Muhammed Roshan <49961411+roshandcoo7@users.noreply.github.com>
2020-06-04 13:43:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+let snake;
+let res = 20;
+let food;
+let w;
+let h;
+
+function setup() {
+  createCanvas(400, 400);
+  w = floor(width / res);
+  h = floor(height / res);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
+}
+
+function draw() {
+  scale(res);
+  background(220);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.gameOver()) {
+    print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
